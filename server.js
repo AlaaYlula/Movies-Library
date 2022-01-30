9f575e5a1723962ffefd513d3dba243cadc3f9e4
AlaaYlula <alaamwazani@hotmail.com>
2022-01-30 23:56:52 +0200

    task11Update

--- server.js
+++ server.js
@@ -3,7 +3,6 @@
 const express = require('express');
 const cors = require('cors');
 const movieData = require('./Movie Data/data.json');
-const { status } = require('express/lib/response');
 
 const app = express();
 app.use(cors());
@@ -12,6 +11,9 @@ app.use(cors());
 app.get('/',handelHomePage);
 app.get('/favorite',handelfavorite);
 app.get('*',handelNotFound);
+
+app.get('/',handleNotServer)
+
 
 function Favorite(title,poster_path, overview){
     this.title= title;
@@ -26,33 +28,36 @@ function Favorite(title,poster_path, overview){
  }
 
 function handelHomePage(req,res){
-   
+    
     let obj =  new Favorite(movieData.title,movieData.poster_path,movieData.overview);
     res.status(200).json(obj);
+    
 }
 
 function handelfavorite(req,res){
-
-    res.status(200).send("Welcome to Favorite Page ");
+    
+    res.status(200).send("Welcome to Favorite Page :) :)");
 
 }
 
 function handelNotFound(req,res){
 
-
-    let obj = new Error(404,"Sorry, something went wrong,page not found error");
+    let obj = new Error(404,"Sorry, something went wrong, page not found error");
     res.status(404).send(obj)
     
 }
 function handleNotServer(req,res)
 {
+    
     if (res.statusCode == 500){
-
-        let obj = new Error(500,"Sorry, something went wrong,Server error");
+        let obj = new Error(500,"Sorry, something went wrong, Server error");
         res.status(500).send(obj)
-        }
+      }
 }
 
-app.listen(3500, ()=>{
-    console.log("listinig to port 3500");
-})
+ app.listen(3000, ()=>{
+    console.log("listinig to port 3000");
+  
+}) 
+
+
